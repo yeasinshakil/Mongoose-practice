0cb1a51c16bcc2a716a77aaa42f641d4b8b122c1
yeasinshakil <shakilrock1998@gmail.com>
2024-03-11 23:30:11 +0600

    second

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -32,3 +32,22 @@ export const getUserbyIdFromDb = async (payload: string) => {
     const user = await User.findOne({ id: payload }, { name: 1, phone: 1 },)
     return user
 }
+/*
+{
+        id: "123456",
+        role: "admin",
+        name: {
+            firstName: "John",
+            middleName: "Doe",
+            lastName: "Doe"
+        },
+        dateOfBirth: "1990-01-01",
+        email: "shakil@gmail.com",
+        password: "shakil1234",
+        gender: "male",
+        phone: "0987654321",
+        emergencyContact: "0987654321",
+        presentAddress: "123 Main Street",
+        permanentAddress: "123 Main Street"
+    }
+*/
